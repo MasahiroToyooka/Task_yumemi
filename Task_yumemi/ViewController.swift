a027c1862d9b286ce6b92c1c6c10d8cd23988fe0
MasahiroToyooka <ehara2481@gmail.com>
2019-09-17 16:27:26 +0900

    Initial Commit

---
+++ Task_yumemi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task_yumemi
+//
+//  Created by 豊岡正紘 on 2019/09/17.
+//  Copyright © 2019 Masahiro Toyooka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
