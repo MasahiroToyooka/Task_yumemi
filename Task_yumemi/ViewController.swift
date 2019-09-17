ccd95bff6b1bffffc2238b182df49a7482c94e72
MasahiroToyooka <ehara2481@gmail.com>
2019-09-17 20:22:37 +0900

    コンポーネントの配置

--- Task_yumemi/ViewController.swift
+++ Task_yumemi/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
